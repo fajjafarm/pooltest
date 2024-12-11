375d1d7ddf15c6490016c261e1283bd87ce077e6
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-11 23:22:20 +0000

    1

--- app/Http/Controllers/PoolTestResultsController.php
+++ app/Http/Controllers/PoolTestResultsController.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\Controller;
 
 class PoolTestResultsController extends Controller {
    public function index() {
+    echo 'hello world';
       $pooltests = DB::select('select * from pool_tests')->get();;
       return view('dashboards.index',compact('pooltests'));
       }
