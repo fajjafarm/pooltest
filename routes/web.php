4330175a210b4f1c35efaa17acec88cab9f950bd
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-11 23:31:55 +0000

    1

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::group(['prefix' => '/', 'middleware' => 'auth'], function () {
     Route::get('{any}', [RoutingController::class, 'root'])->name('any');
 });
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PoolTestResultsController;
 
 Route::get('home','PoolTestResultsController@index');
