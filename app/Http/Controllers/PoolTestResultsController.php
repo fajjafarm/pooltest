e1b485d028d157d697281d12a7c312b48ff5ad0b
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-12 15:05:12 +0000

    2

--- app/Http/Controllers/PoolTestResultsController.php
+++ app/Http/Controllers/PoolTestResultsController.php
@@ -1,66 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-use {{ namespacedModel }};
-use App\Models\lain;
-use Illuminate\Http\Request;
-
-class PoolTestResults extends Controller
+class Controller extends BaseController
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(lain $lain)
-    {
-        //
-    }
+    use AuthorizesRequests, ValidatesRequests;
+}
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(lain $lain)
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request, lain $lain)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(lain $lain, {{ model }} ${{ modelVariable }})
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(lain $lain, {{ model }} ${{ modelVariable }})
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, lain $lain, {{ model }} ${{ modelVariable }})
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(lain $lain, {{ model }} ${{ modelVariable }})
-    {
-        //
-    }
+class PoolTestResultsController extends Controller {
+   public function index() {
+          $pooltests = DB::select('select * from pool_tests')->get();;
+      return view('dashboards.index',compact('pooltests'));
+   
+      }
 }
