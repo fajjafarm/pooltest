893bfbf2681ecf22bfa56455fcc5baa0846da805
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-13 19:27:24 +0000

    1

---
+++ app/Http/Controllers/PoolTestResultsController.php
@@ -0,0 +1,20 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+ 
+class PoolTestResultsController extends Controller
+{
+    /**
+     * Show a list of all of the application's pol test results.
+     */
+    public function index(): View
+    {
+        $pooltestresults = DB::select('select * from pool_tests' );
+ 
+        return view('dashboards.index', ['pooltestresults' => $pooltestresults]);
+    }
+}
