6c6db95cec11f776331e579e18704448468e6f20
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-16 22:16:57 +0000

    `

--- resources/views/pooltests.blade.php
+++ resources/views/pooltests.blade.php
@@ -163,7 +163,7 @@
                                 <h4 class="mb-3">
                                     <span class="ti ti-square-rounded-arrow-down text-success me-1"></span>
                                     <span>
-                     {{$var2}}   
+                     {{$pooltessts}}   
                   
          </span>
                                 </h4>
