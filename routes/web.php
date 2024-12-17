1917137a99383695bc05c495c7c07c5353bd1275
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-17 15:16:29 +0000

    1

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::group(['prefix' => '/', 'middleware' => 'auth'], function () {
     Route::get('{any}', [RoutingController::class, 'root'])->name('any');
 });
 
-Route::get('/pooltests', [PoolTestResultsControll::class, 'pooltestresults' ] )->name('pooltests');
+Route::get('/pooltests', [PoolTestResultsController::class, 'pooltestresults' ] )->name('pooltests');
 
         
