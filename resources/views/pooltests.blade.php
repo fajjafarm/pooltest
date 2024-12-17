5df605b864d10eb7b9bf8fbafe8e691a4800b186
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-17 09:44:12 +0000

    1

--- resources/views/pooltests.blade.php
+++ resources/views/pooltests.blade.php
@@ -163,7 +163,7 @@
                                 <h4 class="mb-3">
                                     <span class="ti ti-square-rounded-arrow-down text-success me-1"></span>
                                     <span>
-                                    @php dd($testresults); @endphp  
+                     {{$pooltestresults}}   
                   
          </span>
                                 </h4>
