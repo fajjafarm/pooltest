f8ad4d5084b4f4fa668ba1fe3fc765e477e53e5d
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-21 00:04:53 +0000

    1

--- database/migrations/0001_01_01_000003_create_equipment_checks_table.php
+++ database/migrations/0001_01_01_000003_create_equipment_checks_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('emergency_equipment_checks', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
             $table->string('location');
             $table->string('equipment_type');
-            $table->string('location');
             $table->date('check_date');
             $table->boolean('status')->default(false); // true for checked, false for not checked
             $table->text('comments')->nullable();
