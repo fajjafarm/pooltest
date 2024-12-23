2483419ef9f240300b7adb208507586b021d4066
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-23 19:48:57 +0000

    1

--- app/Models/PoolTest.php
+++ app/Models/PoolTest.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+public $timestamps = false;
 class PoolTest extends Model
 {
+    
     protected $fillable = ['user_id', 'pool_id', 'dpd1', 'dpd3', 'ccl', 'ph', 'comments'];
 }
