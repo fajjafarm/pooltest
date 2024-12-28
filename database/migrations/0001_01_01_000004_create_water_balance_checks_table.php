a26bba1c3d0d32cbb9cdcbecb8cb3da9875d9f52
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-28 18:35:33 +0000

    2

--- database/migrations/0001_01_01_000004_create_water_balance_checks_table.php
+++ database/migrations/0001_01_01_000004_create_water_balance_checks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWaterBalanceTestsTable2 extends Migration
+class CreateWaterBalanceChecksTable extends Migration
 {
     /**
      * Run the migrations.
