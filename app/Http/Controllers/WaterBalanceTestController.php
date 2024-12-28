ac32f3ce4b1f50abd501ff2e4f7072695ba09409
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-28 18:42:37 +0000

    1

--- app/Http/Controllers/WaterBalanceTestController.php
+++ app/Http/Controllers/WaterBalanceTestController.php
@@ -11,12 +11,15 @@ class WaterBalanceTestController extends Controller
     {
         // Validation would typically go here, e.g., 
         // $request->validate([...]);
+       // $water_balance = $request->input('alkalinity')+$request->input('calcium_hardness'),
 
         $waterTest = new WaterBalanceTest([
             'alkalinity' => $request->input('alkalinity'),
             'calcium_hardness' => $request->input('calcium_hardness'),
             'ph' => $request->input('ph'),
-            'pool_id' => $request->input('pool_id')
+            'pool_id' => $request->input('pool_id'),
+            'water_balance' = $request->input('alkalinity')+$request->input('calcium_hardness');
+            
         ]);
 
         $waterTest->save();
