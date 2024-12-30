7516afd5ae2402254eefc8bbd0eadd95b656fccb
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2024-12-30 19:49:45 +0000

    1

--- app/Models/EmergencyEquipmentCheck.php
+++ app/Models/EmergencyEquipmentCheck.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmergencyEquipmentCheck extends Model
 {
-    protected $fillable = ['equipment_type', 'equipment_location', 'check_date', 'condition', 'comments'];
+    protected $fillable = ['equipment_type', 'equipment_location', 'condition', 'comments'];
 }
