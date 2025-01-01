f027901b0b23a0ce0660848aabc47d805831ec31
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-01 11:41:03 +0000

    1

--- resources/views/pooltest/create.blade.php
+++ resources/views/pooltest/create.blade.php
@@ -6,7 +6,7 @@
     @if(session('success'))  
         <div class="alert alert-success">{{ session('success') }}   </div>
     @endif
-    @if(session('error'))  
+    @if(session('notgood'))  
     <div class="alert alert-danger d-flex align-items-center" role="alert">
                         <iconify-icon icon="solar:danger-triangle-bold-duotone" class="fs-20 me-1"></iconify-icon>
                         <div class="lh-1"><strong>Error - </strong> {{ session('error') }}</div>
