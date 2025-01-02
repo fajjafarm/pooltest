b4feca8d66af9e9f594eb38cbceae1c6cbae7367
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-02 19:27:41 +0000

    1

--- database/migrations/0001_01_01_000006_create_action_logs_table.php
+++ database/migrations/0001_01_01_000006_create_action_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ActionLogsTable extends Migration
+class CreateActionLogsTable extends Migration
 {
     /**
      * Run the migrations.
