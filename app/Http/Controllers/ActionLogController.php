ac80264e2644f064b5af8d6055597996e7c60105
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-03 19:01:44 +0000

    1

--- app/Http/Controllers/ActionLogController.php
+++ app/Http/Controllers/ActionLogController.php
@@ -25,7 +25,8 @@ class ActionLogController extends Controller
         $progressTypes = [
             'Not Started', 'In Progress', 'Unable to Complete', 'Complete'
         ];
-        return view('actionlog.create', compact('testId', 'actionTypes','escalationTypes','progressTypes'));
+        $user_id => auth()->id();
+        return view('actionlog.create', compact('testId', 'actionTypes','escalationTypes','progressTypes','user_id'));
     }
 
     /**
