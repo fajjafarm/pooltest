a8d3ae58fda22c11336b322def156e0ef8332591
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-07 19:48:48 +0000

    1

--- app/Http/Controllers/PoolController.php
+++ app/Http/Controllers/PoolController.php
@@ -10,8 +10,7 @@ class PoolController extends Controller
 {
     public function create($clientID)
     {
-        $companyName = Client::where('client_id', $clientID)->get('company_name');
-        $companyName = implode(",", $companyName);
+        $companyName = Client::where('client_id', $clientID)->first('company_name');
         return view('pools.create',compact('clientID','companyName'));
     }
 
