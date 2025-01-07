5b4bd6b0c3fd34b37b6ef4fb88ec70c8d0ddef3f
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-07 22:46:14 +0000

    Update index.blade.php

--- resources/views/backwashes/index.blade.php
+++ resources/views/backwashes/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.vertical', ['title' => 'Backwash Record for {{ $poolID }}'])
 
 @section('content')
-    @include('layouts.partials.page-title', ['subtitle' => 'Pages', 'title' => 'Backwash Record for '.$poolID])
+    @include('layouts.partials.page-title', ['subtitle' => 'Pages', 'title' => 'Backwash Record for '.$pooName])
     <div class="container mt-5">
    
 @if(session('success'))
