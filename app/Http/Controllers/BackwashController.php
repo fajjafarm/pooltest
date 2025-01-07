a1e5920f2b9f65ad9281c89e407048a3e1e05b38
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-07 22:22:35 +0000

    1

--- app/Http/Controllers/BackwashController.php
+++ app/Http/Controllers/BackwashController.php
@@ -12,7 +12,7 @@ class BackwashController extends Controller
      //   if (isset($poolID)) {
         $backwashes = Backwash::where('pool_id', $poolID)->get();
        // $poolName = PoolList::where('pool_name', $poolID)->get();
-        $poolName = PoolList::where('pool_id', $poolID)->first('pool_name');
+        $poolName = PoolList::where('pool_id', $poolID)->get('pool_name');
         return view('backwashes.index', compact('backwashes','poolID','poolName'));
        // }else {
       //  $backwashes = Backwash::all();
