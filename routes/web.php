ab93e77444a9622fe3f26a91a4a3c972f614a816
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-08 19:22:30 +0000

    1

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/clients', [ClientController::class, 'index'])->name('clients.index'
 Route::get('/clients/create/', [ClientController::class, 'create'])->name('clients.create');
 Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
 
-Route::resource('water-meter-readings', WaterMeterReadingController::class);
+Route::get('water-meter-readings', WaterMeterReadingController::class, 'index');
 
 Route::get('/pools/create/{clientID}', [PoolController::class, 'create'])->name('pools.create');
 Route::post('/pools', [PoolController::class, 'store'])->name('pools.store');
