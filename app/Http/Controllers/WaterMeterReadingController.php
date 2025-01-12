21e05a3a0def38b2cc47a6a09c05ad29439b2575
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-12 22:41:08 +0000

    Update WaterMeterReadingController.php

--- app/Http/Controllers/WaterMeterReadingController.php
+++ app/Http/Controllers/WaterMeterReadingController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\WaterMeterReading;
 use App\Models\PoolList;
-use Illuminate\Http\Request;
 use App\Charts\WaterBalanceChart;
+use Illuminate\Http\Request;
+
 
 
 class WaterMeterReadingController extends Controller
 {
-    public function index($poolID, $chart)
+    public function index($poolID)
     {
         $readings = WaterMeterReading::where('plantroom_id', $poolID)->orderBy('created_at', 'desc')->get();
         // $poolName = PoolList::where('pool_name', $poolID)->get();
         $poolID=$poolID;
+        $chart = $chart;
          $poolName = PoolList::where('pool_id', $poolID)->value('pool_name');
         $chart = $chart->build($poolID) ;
            return view('water-meter-readings.index', compact('readings' ,'poolID','poolName','chart'));
