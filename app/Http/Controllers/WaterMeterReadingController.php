36f32a77fbc442736d19b4db50056f86e32038f4
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-12 22:36:31 +0000

    Update WaterMeterReadingController.php

--- app/Http/Controllers/WaterMeterReadingController.php
+++ app/Http/Controllers/WaterMeterReadingController.php
@@ -16,7 +16,7 @@ class WaterMeterReadingController extends Controller
         // $poolName = PoolList::where('pool_name', $poolID)->get();
         $poolID=$poolID;
          $poolName = PoolList::where('pool_id', $poolID)->value('pool_name');
-        $chart => $chart->build($poolID) ;
+        $chart = $chart->build($poolID) ;
            return view('water-meter-readings.index', compact('readings' ,'poolID','poolName','chart'));
     }
     public function create($poolID)
