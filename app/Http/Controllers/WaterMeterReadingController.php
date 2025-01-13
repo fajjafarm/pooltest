aaf33f4a8e7d6eb676bc015ef03b53d04817e92c
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-13 23:09:29 +0000

    Update WaterMeterReadingController.php

--- app/Http/Controllers/WaterMeterReadingController.php
+++ app/Http/Controllers/WaterMeterReadingController.php
@@ -19,7 +19,7 @@ class WaterMeterReadingController extends Controller
         $poolName = PoolList::where('pool_id', $poolID)->value('pool_name');
         $watermeterreadings = WaterMeterReading::where('plantroom_id', $poolID)->orderBy('created_at', 'asc')->get();
         $i=0;
-        $diffs[$i] = array();
+        $diffs[] = array();
         $count = 0;
         $prevNum =0;
 $diffs = 0;
