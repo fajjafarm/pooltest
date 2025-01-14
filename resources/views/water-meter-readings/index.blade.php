42462452f7d04a82c4a10b73ab931c3f0fa8719e
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-14 18:51:40 +0000

    Update index.blade.php

--- resources/views/water-meter-readings/index.blade.php
+++ resources/views/water-meter-readings/index.blade.php
@@ -56,7 +56,7 @@
     <tbody>
     
    
-    @foreach array_combine($readings , $diffs as $reading)
+    @foreach ($readings , $diffs as $reading)
         <tr>
             <td>
             {{ $reading->meter_reading }}    
