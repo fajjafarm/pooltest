0ebf8e84cd1c1009324ec5508a0607ed116973f6
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-14 18:56:11 +0000

    Update index.blade.php

--- resources/views/water-meter-readings/index.blade.php
+++ resources/views/water-meter-readings/index.blade.php
@@ -62,7 +62,7 @@
             {{ $reading->meter_reading }}    
             </td>
             <td>
-            {{ $diffs['$key'] }}
+            {{ $diffs[$key] }}
             </td>
             <td>
 Info
