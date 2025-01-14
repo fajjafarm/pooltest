5b4c9285000995ffdf0631d35d1ee11f159aa4d8
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-14 19:12:41 +0000

    Update WaterMeterReadingController.php

--- app/Http/Controllers/WaterMeterReadingController.php
+++ app/Http/Controllers/WaterMeterReadingController.php
@@ -63,7 +63,7 @@ foreach($watermeterreadings as $i =>$watermeterreading) {
 
     public function store(Request $request)
     {
-        $latestreading = WaterMeterReading::pluck('meter_reading', $poolID)->orderBy('created_at', 'desc')->get();
+        $latestreading = WaterMeterReading::pluck('meter_reading', $request->input('plantroom_id'))->orderBy('created_at', 'desc')->get();
         $diff = $request->input('meter_reading')-$latestreading;
         WaterMeterReading::create($request->all()+ ['logged_by' => auth()->id()]+ ['difference' => $diff]);
         return redirect()->route('water-meter-readings.index',[$request->input('pool_id')])->with('success', 'Reading logged successfully.');
