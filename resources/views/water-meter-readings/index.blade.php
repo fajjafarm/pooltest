777f80750a9ec0b824f5e81ce965103422433575
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-15 20:02:56 +0000

    Update index.blade.php

--- resources/views/water-meter-readings/index.blade.php
+++ resources/views/water-meter-readings/index.blade.php
@@ -67,7 +67,7 @@
             <td>
             @if($reading->difference < '999')<button type="button" class="btn btn-soft-info">Low Water Usage</button>@endif
             @if($reading->difference >= '1000' && $reading->difference < '2500')<button type="button" class="btn btn-soft-success">Normal Water Usage</button>@endif 
-            @if($reading->difference >= '2500')<button type="button" class="btn btn-soft-danger">Abnormal Water Usage</button>>@endif   
+            @if($reading->difference >= '2500')<button type="button" class="btn btn-soft-danger">Abnormal Water Usage</button>@endif   
             </td>
             <td> <button type="button" tabindex="0" class="btn btn-info" data-bs-toggle="popover" data-bs-placement="left" data-bs-trigger="focus" data-bs-content="{{ $reading->logged_by }}" data-bs-original-title="Reading Logged By">
             <iconify-icon icon="solar:people-nearby-broken" class="fs-20 me-1" alt="{{ $reading->logged_by }}"></iconify-icon>
