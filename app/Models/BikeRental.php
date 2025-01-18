4f2401211cf9123b208ddadcf3eb832115739264
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-18 19:55:43 +0000

    Update BikeRental.php

--- app/Models/BikeRental.php
+++ app/Models/BikeRental.php
@@ -1,12 +1,24 @@
 <?php 
 // app/Models/Rental.php
 namespace App\Models;
+use Illuminate\Support\Str;
 
 use Illuminate\Database\Eloquent\Model;
 
 class BikeRental extends Model
 {
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = (string) Str::ulid();
+        });
+    }
     protected $fillable = ['bike_id', 'order_number', 'name', 'duration', 'helmet_borrowed', 'lock_borrowed'];
+    
 
     public function bike()
     {
