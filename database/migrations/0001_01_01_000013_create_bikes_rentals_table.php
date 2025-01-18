50f559cc0c2014967ff575ccb18e9c1e2102fe48
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-18 19:35:51 +0000

    Update 0001_01_01_000013_create_bikes_rentals_table.php

--- database/migrations/0001_01_01_000013_create_bikes_rentals_table.php
+++ database/migrations/0001_01_01_000013_create_bikes_rentals_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 
-class CreateRentalsTable extends Migration
+class CreateBikeRentalsTable extends Migration
 {
     public function up()
     {
