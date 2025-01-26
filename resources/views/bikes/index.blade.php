6bf4473dadb7794951928fa01b066162ebb9f24f
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-01-26 18:56:23 +0000

    Update index.blade.php

--- resources/views/bikes/index.blade.php
+++ resources/views/bikes/index.blade.php
@@ -122,11 +122,11 @@
             @method('PUT')
             <div class="input-group">
             <select class="form-select" name="status" >
-                <option value="ready for hire">Ready for Hire</option>
-                <option value="hired">Hired</option>
-                <option value="awaiting check">Awaiting Check</option>
-                <option value="awaiting maintenance">Maintenance</option>
-                <option value="reserved">Reserved</option>
+            <option value="ready for hire" @if($ebike->status == 'ready for hire') selected @endif>Ready for Hire</option>
+                <option value="hired" @if($ebike->status == 'hired') selected @endif>Hired</option>
+                <option value="awaiting check" @if($ebike->status == 'awaiting check') selected @endif>Awaiting Check</option>
+                <option value="awaiting maintenance" @if($ebike->status == 'awaiting maintenance') selected @endif>Maintenance</option>
+                <option value="reserved" @if($ebike->status == 'reserved') selected @endif>Reserved</option>
             </select>
             <button class="btn btn-outline-secondary" type="submit">Update</button></div>
         </form></td>
