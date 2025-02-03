e3c30a7618f25ce718674868b7ead5c68a65a737
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-02-03 18:26:37 +0000

    Update WaterMeterReadingController.php

--- app/Http/Controllers/WaterMeterReadingController.php
+++ app/Http/Controllers/WaterMeterReadingController.php
@@ -43,7 +43,7 @@ foreach($watermeterreadings as $i =>$watermeterreading) {
         }
         $chart = $chart->barChart()
         ->setTitle('Litres')
-        ->addData('litres',  $litres)
+        ->addData('litres',  $watermeterreading['meter_reading'])
         ->setXAxis( $labels);
         
            return view('water-meter-readings.index', compact('readings' ,'poolID','poolName','chart','labels','litres','diffs'));
