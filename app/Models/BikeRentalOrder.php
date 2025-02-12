f5493bc5c45854f0aed214abf13e1f41d3a9ce4f
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-02-12 18:40:10 +0000

    Create BikeRentalOrder.php

---
+++ app/Models/BikeRentalOrder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BikeRentalOrder extends Model
+{
+    use \Illuminate\Database\Eloquent\Concerns\HasUlids; // This trait adds ULID support
+
+    protected $table = 'bike_rental_orders';
+    protected $fillable = [
+        'order_number', 'duration', 'date', 'bike_numbers', 'helmet_numbers', 'lock_numbers', 'status'
+    ];
+
+    protected $casts = [
+        'bike_numbers' => 'array',
+        'helmet_numbers' => 'array',
+        'lock_numbers' => 'array',
+        'date' => 'date',
+    ];
+}
