edef58f97381d7ef25dc4d0c0fc75b7ded94a5ea
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-02-14 23:45:01 +0000

    Update index.blade.php

--- resources/views/bike-rental-orders/index.blade.php
+++ resources/views/bike-rental-orders/index.blade.php
@@ -34,7 +34,7 @@
 </tr>
 <tbody>
 @foreach ($orders as $order)
-<tr> <form method="PUT" action="{{ route('bike-rental-orders.update') }}">
+<tr> <form method="PUT" action="{{ route('bike-rental-orders.index') }}">
 <input type="hidden" value="{{$order->order_number}}" name="id">
             @csrf
             
