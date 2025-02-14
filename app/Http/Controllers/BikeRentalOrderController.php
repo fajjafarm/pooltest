279a85333872aa5f9650be803cf40f4cf0651d94
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-02-14 23:28:36 +0000

    Update BikeRentalOrderController.php

--- app/Http/Controllers/BikeRentalOrderController.php
+++ app/Http/Controllers/BikeRentalOrderController.php
@@ -38,9 +38,9 @@ class BikeRentalOrderController extends Controller
         $locks = BikeLocks::readyToHire();
         $bikes = Bike::readyToHire();
         $orders = BikeRentalOrder::all();
-       // return view('bike-rental-orders.index', compact('orders', 'bikes', 'helmets', 'locks'));
+        return view('bike-rental-orders.index', compact('orders', 'bikes', 'helmets', 'locks'));
       // return view('bike-rental-orders.index');
-        return redirect()->back()->with('status', 'bike orders updated!');
+       // return redirect()->back()->with('status', 'bike orders updated!');
        // return response()->json($order);
     }
     public function index()
