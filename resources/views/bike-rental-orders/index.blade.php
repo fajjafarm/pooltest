d806d0a5834b718393c2299bf2d8fdaf2807207d
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-02-14 23:39:56 +0000

    Update index.blade.php

--- resources/views/bike-rental-orders/index.blade.php
+++ resources/views/bike-rental-orders/index.blade.php
@@ -37,7 +37,7 @@
 <tr> <form method="POST" action="{{ route('bike-rental-orders.update') }}">
 <input type="hidden" value="{{$order->order_number}}" name="id">
             @csrf
-            @method('PUT')
+            
                         <td>{{ $order->order_number }}</td>
                         <td>{{ $order->duration }} hours</td>
                         <td>{{ $order->date->format('d-m-Y') }}</td>
