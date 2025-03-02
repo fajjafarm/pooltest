7481d7ad22fa16f310e6fd884a18761dde98b48c
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-02 18:27:05 +0000

    Update sidenav.blade.php

--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -40,15 +40,8 @@
             </li>
 
             
-            <li class="side-nav-title">Thermal Suites</li>
-            <li class="side-nav-item">
-                    <a data-bs-toggle="collapse" href="#sidebarThermalChecks" aria-expanded="false" aria-controls="sidebarThermalChecks" class="side-nav-link">
-                    <span class="menu-icon"><i class="ti ti-dashboard"></i></span>
-                    <span class="menu-text">Record a Check</span>
-                    <span class="menu-arrow"></span>
-                </a>
-                <div class="collapse" id="sidebarThermalSuite">
-                    <ul class="sub-menu">
+            <li class="side-nav-title">Thermal Suite Checks</li>
+     
                         <li class="side-nav-item">
                             <a href="{{ route('thermal_suite_checks.create', '01JNBZ3KEY3WX9K92ZCE2H6RXC') }}" class="side-nav-link">
                                 <span class="menu-text">Resort Thermal Suite</span>
@@ -59,8 +52,8 @@
                                 <span class="menu-text">Hotel Thermal Suite</span>
                             </a>
 </li>                      
-                        </ul>
-                </div>
+                       
+                
             </li>
             <ul class="side-nav">
             <li class="side-nav-title">Plant Room</li>
