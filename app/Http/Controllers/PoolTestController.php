47b0cb3665d1cffdfd6f55035b5120d3bcd028fc
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-09 19:59:43 +0000

    Update PoolTestController.php

--- app/Http/Controllers/PoolTestController.php
+++ app/Http/Controllers/PoolTestController.php
@@ -32,7 +32,7 @@ class PoolTestController extends Controller
 
             $validated['ccl'] = $request->dpd3 - $request->dpd1;
             $validated['user_id'] = auth()->id() ?? throw new \Exception('User not authenticated');
-            dd($validated)
+            dd($validated);
 
 
             PoolTest::create($validated);
