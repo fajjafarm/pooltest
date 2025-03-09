8650b6b2ea34539262a5dd588f494546c198a463
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-09 22:52:36 +0000

    1

--- app/Models/PoolTest.php
+++ app/Models/PoolTest.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Support\Str;
 
 class PoolTest extends Model
 {
     use HasFactory;
+    use HasFactory, HasUlids;
 
-    /**
+/**
      * The table associated with the model.
      *
      * @var string
@@ -18,6 +20,28 @@ class PoolTest extends Model
     protected $table = 'pool_tests';
 
     /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+
+   /**
      * The attributes that are mass assignable.
      *
      * @var array
