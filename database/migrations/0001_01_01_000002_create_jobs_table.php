4eeb07ef1d81b0c6b219bf4d79398e8c4b2c1697
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-20 19:29:23 +0000

    Update 0001_01_01_000002_create_jobs_table.php

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -11,18 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('emergency_equipment_checks', function (Blueprint $table) {
-            $table->id();
-            $table->string('location');
-            $table->string('equipment_type');
-            $table->string('location');
-            $table->date('check_date');
-            $table->boolean('status')->default(false); // true for checked, false for not checked
-            $table->text('comments')->nullable();
-            $table->string('checked_by');
-            $table->timestamps();
-        });
-    
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->string('queue')->index();
