24cb420ad0ec3c99ee381f5321c24150c7c2f78a
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-21 11:49:09 +0000

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -105,7 +105,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may define the amount of seconds before a password confirmation
-    | window expires and users are asked to re-enter their password via the
+    | window expires and users are asked to re-9enter their password via the
     | confirmation screen. By default, the timeout lasts for three hours.
     |
     */
